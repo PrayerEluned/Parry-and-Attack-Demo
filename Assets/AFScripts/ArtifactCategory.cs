4a26bc06ac5c618ecff178fa56c02a71563ff97f
PlayerEluned <yoshihara.chorus@gamil.com>
2025-09-02 17:27:02 +0900

    Stop tracking Assets/ in repo (.gitignore + rm --cached)

--- Assets/AFScripts/ArtifactCategory.cs
+++
@@ -1,9 +0,0 @@
-public enum ArtifactCategory
-{
-    Weapon,
-    Armor,
-    Accessory,
-    Relic,
-    Gem,
-    Other
-}
--- Assets/GameCore/ScriptableObjects/PrefabDatabase.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PrefabDatabase", menuName = "GameCore/PrefabDatabase")]
-public class PrefabDatabase : ScriptableObject
-{
-    public GameObject[] prefabs;
-}
